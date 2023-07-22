de381463138d75ea3ca93abb3d3bd0919e6bd9f1
shivaniarora-developer <77771205+shivaniarora-developer@users.noreply.github.com>
2023-07-22 16:13:35 +0530

    Update Six.tsx

--- app/components/Six.tsx
+++ app/components/Six.tsx
@@ -6,8 +6,8 @@ export default function Six() {
     return (
 <section className="text-gray-600 body-font relative">
   <div className="absolute inset-0 bg-gray-300">
-    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="/gallery1.jpg" style={{filter: "grayscale(1) contrast(1.2) opacity(0.4)"}}></iframe>
-  </div>
+    <Image width="1068" height="400" alt="" src="/gallery1.jpg" />
+  </div> 
   <div className="container px-5 py-24 mx-auto flex">
     <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
       <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
