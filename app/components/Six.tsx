a51ca0ae416030fdd7f21ec75dec6acc72b4e398
Your Name <you@example.com>
2023-07-22 15:21:21 +0530

    first commit

--- app/components/Six.tsx
+++ app/components/Six.tsx
@@ -6,7 +6,7 @@ export default function Six() {
     return (
 <section className="text-gray-600 body-font relative">
   <div className="absolute inset-0 bg-gray-300">
-    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" style={{filter: "grayscale(1) contrast(1.2) opacity(0.4)"}}></iframe>
+    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="/gallery1.jpg" style={{filter: "grayscale(1) contrast(1.2) opacity(0.4)"}}></iframe>
   </div>
   <div className="container px-5 py-24 mx-auto flex">
     <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
